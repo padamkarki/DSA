16a286a9c05211444fef9ad48dca8e44ff6d7d16
Padam Karki <padamkarkipkkp@gmail.com>
2024-06-05 10:43:17 +0530

    Big O start

---
+++ Big_O.js
@@ -0,0 +1,11 @@
+const nemo = ["nemo"];
+
+function findNemo(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "nemo") {
+      console.log("Found Nemo!");
+    }
+  }
+}
+
+findNemo(nemo);
