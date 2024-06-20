7da8619778b8490329f1b2e549afdaea4b52431d
Padam Karki <padamkarkipkkp@gmail.com>
2024-06-20 16:56:57 +0530

    docs added on validate bst

--- Searching/validate_bst.js
+++ Searching/validate_bst.js
@@ -3,9 +3,13 @@ var isValidBST = function (root) {
 };
 
 function validate(node, low, high) {
+  // An empty tree is a valid BST
+
   if (node === null) {
     return true;
   }
+  // Early exit condition if not bst
+  // The current node's value must be within the range defined by low and high
 
   if (
     (low !== null && node.val <= low) ||
@@ -14,6 +18,9 @@ function validate(node, low, high) {
     return false;
   }
 
+  // Recursively validate the left and right subtrees
+  // The left subtree must be less than the current node's value
+  // The right subtree must be greater than the current node's value
   return (
     validate(node.left, null, node.val) && validate(node.right, node.val, high)
   );
